883005e16ff1a542975bec420ab7fd0041db78c1
Giuseppe Fierro <gspe@ae-design.ws>
2016-06-02 23:00:33 +0200

    Removed link environment variables reference from docker-compose.yml

--- nginx/start.sh
+++ nginx/start.sh
@@ -5,18 +5,18 @@ echo build starting nginx config
 
 
 echo replacing ___my.example.com___/$MY_DOMAIN_NAME
-echo replacing ___LETSENCRYPT_IP___/$LETSENCRYPT_PORT_80_TCP_ADDR
-echo replacing ___LETSENCRYPT_PORT___/$LETSENCRYPT_PORT_80_TCP_PORT
-echo replacing ___APPLICATION_IP___/$APP_PORT_80_TCP_ADDR
-echo replacing ___APPLICATION_PORT___/$APP_PORT_80_TCP_PORT
+echo replacing ___LETSENCRYPT_IP___/$LETSENCRYPT_IP
+echo replacing ___LETSENCRYPT_PORT___/$LETSENCRYPT_PORT_80
+echo replacing ___APPLICATION_IP___/$APPLICATION_IP
+echo replacing ___APPLICATION_PORT___/$APPLICATION_PORT
 
 # Put your domain name into the nginx reverse proxy config.
 sed -i "s/___my.example.com___/$MY_DOMAIN_NAME/g" /etc/nginx/nginx.conf
 # Add your app's container IP and port into config
-sed -i "s/___APPLICATION_IP___/$APP_PORT_80_TCP_ADDR/g" /etc/nginx/nginx.conf
-sed -i "s/___APPLICATION_PORT___/$APP_PORT_80_TCP_PORT/g" /etc/nginx/nginx.conf
-sed -i "s/___LETSENCRYPT_IP___/$LETSENCRYPT_PORT_80_TCP_ADDR/g" /etc/nginx/nginx.conf
-sed -i "s/___LETSENCRYPT_PORT___/$LETSENCRYPT_PORT_80_TCP_PORT/g" /etc/nginx/nginx.conf
+sed -i "s/___APPLICATION_IP___/$APPLICATION_IP/g" /etc/nginx/nginx.conf
+sed -i "s/___APPLICATION_PORT___/$APPLICATION_PORT/g" /etc/nginx/nginx.conf
+sed -i "s/___LETSENCRYPT_IP___/$LETSENCRYPT_IP/g" /etc/nginx/nginx.conf
+sed -i "s/___LETSENCRYPT_PORT___/$LETSENCRYPT_PORT_80/g" /etc/nginx/nginx.conf
 
 cat /etc/nginx/nginx.conf
 echo .
@@ -50,26 +50,26 @@ do
 done
 
 echo replacing ___my.example.com___/$MY_DOMAIN_NAME
-echo replacing ___LETSENCRYPT_IP___/$LETSENCRYPT_PORT_80_TCP_ADDR
-echo replacing ___LETSENCRYPT_PORT___/$LETSENCRYPT_PORT_80_TCP_PORT
-echo replacing ___LETSENCRYPT_HTTPS_IP___/$LETSENCRYPT_PORT_443_TCP_ADDR
-echo replacing ___LETSENCRYPT_HTTPS_PORT___/$LETSENCRYPT_PORT_443_TCP_PORT
-echo replacing ___APPLICATION_IP___/$APP_PORT_80_TCP_ADDR
-echo replacing ___APPLICATION_PORT___/$APP_PORT_80_TCP_PORT
+echo replacing ___LETSENCRYPT_IP___/$LETSENCRYPT_IP
+echo replacing ___LETSENCRYPT_PORT___/$LETSENCRYPT_PORT_80
+echo replacing ___LETSENCRYPT_HTTPS_IP___/$LETSENCRYPT_IP
+echo replacing ___LETSENCRYPT_HTTPS_PORT___/$LETSENCRYPT_PORT_443
+echo replacing ___APPLICATION_IP___/$APPLICATION_IP
+echo replacing ___APPLICATION_PORT___/$APPLICATION_PORT
 
 
 # Put your domain name into the nginx reverse proxy config.
 sed -i "s/___my.example.com___/$MY_DOMAIN_NAME/g" /etc/nginx/nginx-secure.conf
 
 # Add LE container IP and port into config
-sed -i "s/___LETSENCRYPT_IP___/$LETSENCRYPT_PORT_80_TCP_ADDR/g" /etc/nginx/nginx-secure.conf
-sed -i "s/___LETSENCRYPT_PORT___/$LETSENCRYPT_PORT_80_TCP_PORT/g" /etc/nginx/nginx-secure.conf
-sed -i "s/___LETSENCRYPT_HTTPS_IP___/$LETSENCRYPT_PORT_443_TCP_ADDR/g" /etc/nginx/nginx-secure.conf
-sed -i "s/___LETSENCRYPT_HTTPS_PORT___/$LETSENCRYPT_PORT_443_TCP_PORT/g" /etc/nginx/nginx-secure.conf
+sed -i "s/___LETSENCRYPT_IP___/$LETSENCRYPT_IP/g" /etc/nginx/nginx-secure.conf
+sed -i "s/___LETSENCRYPT_PORT___/$LETSENCRYPT_PORT_80/g" /etc/nginx/nginx-secure.conf
+sed -i "s/___LETSENCRYPT_HTTPS_IP___/$LETSENCRYPT_IP/g" /etc/nginx/nginx-secure.conf
+sed -i "s/___LETSENCRYPT_HTTPS_PORT___/$LETSENCRYPT_PORT_443/g" /etc/nginx/nginx-secure.conf
 
 # Add your app's container IP and port into config
-sed -i "s/___APPLICATION_IP___/$APP_PORT_80_TCP_ADDR/g" /etc/nginx/nginx-secure.conf
-sed -i "s/___APPLICATION_PORT___/$APP_PORT_80_TCP_PORT/g" /etc/nginx/nginx-secure.conf
+sed -i "s/___APPLICATION_IP___/$APPLICATION_IP/g" /etc/nginx/nginx-secure.conf
+sed -i "s/___APPLICATION_PORT___/$APPLICATION_PORT/g" /etc/nginx/nginx-secure.conf
 
 #go!
 kill $(ps aux | grep '[n]ginx' | awk '{print $2}')
